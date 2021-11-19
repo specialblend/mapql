8186adcb4cbff14092e5e09b302e5646dfcb1931
specialblend <cj@specialblend.dev>
2021-11-18 20:48:58 -0600

    refactor: add discriminant types of Exec

--- src/contract.ts
+++ src/contract.ts
@@ -1,31 +1,31 @@
-import { ExecInfo } from "graphql-anywhere";
+import type { ExecInfo } from "graphql-anywhere";
+import type { DirectiveInfo } from "apollo-utilities/src/directives";
+
 import { DIRECTIVES } from "./transform";
 
 export type Maybe<T> = T | undefined;
+export type PathSelector = string | "@";
 
 export type JsonParent = JsonRecord | JsonList;
 export type JsonChild = string | number | boolean | null | JsonParent;
 export type JsonRecord = { [k: string]: JsonChild };
 export type JsonList = JsonChild[];
-
 export type JsonSelector = JsonChild | undefined;
-
-export type PathSelector = string | "@";
 
 export interface Filter {
   from?: PathSelector;
   match?: JsonSelector;
   noMatch?: JsonSelector;
 }
+
 export type ExecRoot = any;
+export type ExecCtx = any;
+export type ExecSource = JsonRecord;
+
 export interface ExecArgs {
   from?: PathSelector;
   filter?: Filter;
 }
-export type ExecCtx = any;
-export type ExecSource = JsonRecord;
-export type ExecParent = any;
-export type ExecChild = JsonChild;
 
 export type Exec = {
   fieldName: string;
@@ -33,9 +33,24 @@ export type Exec = {
   args: ExecArgs;
   context: ExecCtx;
   info: ExecInfo;
-  data?: ExecSource;
-  parent?: ExecParent;
-  child?: ExecChild;
+};
+
+export type ExecInfoConst = ExecInfo & {
+  directives: DirectiveInfo & {
+    const: {
+      of: string;
+    };
+  };
+};
+
+export type HasConst = Exec & {
+  info: ExecInfoConst;
+};
+
+export type HasFilter = Exec & {
+  args: ExecArgs & {
+    filter: Filter;
+  };
 };
 
 export type DirectiveMap = typeof DIRECTIVES;
